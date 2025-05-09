6428add0c553830ca3a82ae12c1148ae9594651f
yanswo <yanlucas2202@gmail.com>
2025-05-09 10:22:04 -0300

    WS - 2: Rotas.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PrismaClient } from "../../generated/prisma/index.js"; // Ajuste o caminho se necessário
+import { PrismaClient } from "../../generated/prisma/index.js"; 
 
 const router = express.Router()
 
